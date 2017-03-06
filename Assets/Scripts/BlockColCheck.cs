1fa70778084435911c8357103c326f38d79c621e
Lee JangKeun <bbh1039@naver.com>
2017-03-07 01:36:26 +0900

    3 -1 Project
    - Animation Complet
    - Block Added
    - Puzzle Maked
    
    ##############
    ## Need To Fix ##
    ##############
    
    - Block Anchor

---
+++ Assets/Scripts/BlockColCheck.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BlockColCheck : MonoBehaviour {
+
+    protected SpriteRenderer m_SR;
+    protected abstract void OnBlockCollition();
+
+    void Awake()
+    {
+        m_SR.GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //Debug.Log("Touched");
+            OnBlockCollition();
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/OnNumBlockHit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnNumBlockHit : BlockColCheck
+{
+    public Sprite [] sp = new Sprite[10];
+    public GameObject Gate1;
+
+    int i = 1;
+
+    void Awake()
+    {
+        m_SR = GetComponent<SpriteRenderer>();
+    }
+	
+	void FixedUpdate ()
+    {
+	    if (i == 9)
+        {
+            Destroy(Gate1);
+        }
+	}
+
+    protected override void OnBlockCollition()
+    {
+        m_SR.sprite = sp[i];
+
+        if (i == 9) i = 0;
+        else i++;
+    }
+}
