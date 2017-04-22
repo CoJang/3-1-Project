99c09f7215a93fa1472ac60d2f66474acbb1bd75
Lee JangKeun <bbh1039@naver.com>
2017-04-23 01:55:40 +0900

    3-1 Project : Math Island :)
    - Added Monster Spine Animation & Mech.s
    - Still Can Get Blocks At The Same Time...
    - Delete Some Lagacy Files
    - Added Title Scene
    - Added ClearGate (Image Only)
    
    - Graphic Reasource Changed [ 85 % ]
    
    ## Will Fix ##
    # (Important) Jump Touch Bug
    # (Important) Item Pickup Bug
    # Organization [Serialziedfield]
    
    ## Will Add ##
    # Fixed Camera Ratio (by Script)
    # Use Item Mech.
    # Stage UI ( Even Animations? )

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    SpriteRenderer TouchTheScreen;
+
+    void Awake()
+    {
+        TouchTheScreen = GetComponentInChildren<SpriteRenderer>();
+        StartCoroutine(Blink());
+    }
+
+    IEnumerator Blink()
+    {
+        int CountTime = 0;
+
+        while (true)
+        {
+            if (CountTime % 2 == 0)
+                TouchTheScreen.color = new Color32(255, 255, 255, 150);
+            else
+                TouchTheScreen.color = new Color32(255, 255, 255, 255);
+
+            yield return new WaitForSeconds(0.75f);
+
+            CountTime++;
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ChangeIntoGameScene();
+        }
+    }
+
+    public void ChangeIntoGameScene()
+    {
+        SceneManager.LoadScene("TutorialMap");
+    }
+    
+}
